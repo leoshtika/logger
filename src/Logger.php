36088169b936e77c30b19bf5d1463f65c8b3e957
Leonard PC <leoshtika@gmail.com>
2014-09-21 17:27:30 +0300

    Format code

--- src/Logger.php
+++ src/Logger.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author Leonard Shtika <leonard@shtika.info>
  * @link http://leonard.shtika.info
@@ -17,71 +18,71 @@ namespace leoshtika\libs;
  * use leoshtika\libs\Logger;
  * Logger::add('Your message', Logger::LEVEL_WARNING);
  */
-class Logger 
+class Logger
 {
-	// Path where the logfile will be saved.
-	const LOGS_FOLDER_PATH = 'logfiles/';
-	
-	// Level codes for logs
-	const LEVEL_EMERGENCY   = 'EMERGENCY';
-	const LEVEL_ALERT       = 'ALERT';
-	const LEVEL_CRITICAL    = 'CRITICAL';
-	const LEVEL_ERROR       = 'ERROR';
-	const LEVEL_WARNING     = 'WARNING';
-	const LEVEL_NOTICE      = 'NOTICE';
-	const LEVEL_INFO        = 'INFO';
-	const LEVEL_DEBUG       = 'DEBUG';
+    // Path where the logfile will be saved.
+    const LOGS_FOLDER_PATH = 'logfiles/';
     
-	// File handler  
-	private static $_fileHandler = null;
-	
-	
-    /**
-	 * Write message to the log file
-	 * @param string $message Optional message
-	 * @param string $logCode Optional log code (example: INFO (default), ERROR, ATTEMPT TO HACK, ...)
-	 */
-	public static function add($message='no message', $logCode=self::LEVEL_INFO)
-	{
-		// if file pointer doesn't exist, then open log file  
-		if (!self::$_fileHandler)
-		{
-			self::_openLogFile();
-		}
+    // Level codes for logs
+    const LEVEL_EMERGENCY = 'EMERGENCY';
+    const LEVEL_ALERT = 'ALERT';
+    const LEVEL_CRITICAL = 'CRITICAL';
+    const LEVEL_ERROR = 'ERROR';
+    const LEVEL_WARNING = 'WARNING';
+    const LEVEL_NOTICE = 'NOTICE';
+    const LEVEL_INFO = 'INFO';
+    const LEVEL_DEBUG = 'DEBUG';
 
-		// Get user IP
-		$userIP = $_SERVER['REMOTE_ADDR'];
-		
-		// define current time  
-		$time = date('j/M/Y H:i:s');
-		
-		// define script name  
-		$scriptName = pathinfo($_SERVER['PHP_SELF'], PATHINFO_BASENAME);
-		
-		// write message to the log file  
-		fwrite(self::$_fileHandler, "$userIP [$time][$logCode] ($scriptName) --> $message\n");
-	}
-	
-	
-	/**
-	 * Open log file
-	 */
-	private static function _openLogFile()
-	{
-		// define the current date (it will be appended to the log file name)  
-		$month = date('Y-m');
-        
+    // File handler  
+    private static $_fileHandler = null;
+
+
+    /**
+     * Write message to the log file
+     * @param string $message Optional message
+     * @param string $logCode Optional log code (example: INFO (default), ERROR, EMERGENCY)
+     */
+    public static function add($message = 'No message', $logCode = self::LEVEL_INFO)
+    {
+        // if file pointer doesn't exist, then open log file  
+        if (!self::$_fileHandler)
+        {
+            self::_openLogFile();
+        }
+
+        // Get user IP
+        $userIP = $_SERVER['REMOTE_ADDR'];
+
+        // define current time  
+        $time = date('j/M/Y H:i:s');
+
+        // define script name  
+        $scriptName = pathinfo($_SERVER['PHP_SELF'], PATHINFO_BASENAME);
+
+        // write message to the log file  
+        fwrite(self::$_fileHandler, "$userIP [$time][$logCode] ($scriptName) --> $message\n");
+    }
+
+
+    /**
+     * Open log file
+     */
+    private static function _openLogFile()
+    {
+        // define the current date (it will be appended to the log file name)  
+        $month = date('Y-m');
+
         self::_crateLogsFolder();
 
         // define log file path and name  
-		$logFile = self::LOGS_FOLDER_PATH.$month.'.log';
-		
-		// open log file for writing only; place the file pointer at the end of the file  
-		// if the file does not exist, attempt to create it  
-		self::$_fileHandler = fopen($logFile, 'a') or exit("Can't open $logFile!");
-	}
-    
-    
+        $logFile = self::LOGS_FOLDER_PATH . $month . '.log';
+
+        // open log file for writing only; place the file pointer at the end of the file  
+        // if the file does not exist, attempt to create it  
+        self::$_fileHandler = fopen($logFile, 'a') or exit("Can't open $logFile!");
+    }
+
+
     /**
      * Create LogsFolder if not exist
      * This method will create a .htaccess to deny access to logfiles
@@ -92,9 +93,9 @@ class Logger
         if (!file_exists(self::LOGS_FOLDER_PATH))
         {
             mkdir(self::LOGS_FOLDER_PATH, 0755, true);
-            $htaccessHandler = fopen(self::LOGS_FOLDER_PATH.'.htaccess', 'w');
+            $htaccessHandler = fopen(self::LOGS_FOLDER_PATH . '.htaccess', 'w');
             fwrite($htaccessHandler, 'deny from all');
             fclose($htaccessHandler);
-        } 
+        }
     }
 }
